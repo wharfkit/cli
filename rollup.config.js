1f4cd263bb3314216c5bc8e81d1bd629ccd5cb1e
Aaron Cox <aaron@greymass.com>
2023-08-28 16:50:36 -0700

    Reworked shebang to disable experimental warnings

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,27 @@
 import typescript from '@rollup/plugin-typescript'
 import cleanup from 'rollup-plugin-cleanup'
 import json from '@rollup/plugin-json'
-import shebang from 'rollup-plugin-add-shebang';
 
 import pkg from './package.json'
 
 const external = ['fs', ...Object.keys(pkg.dependencies)]
 
+// Add shebang + disable experimental fetch warning
+const banner = `
+#!/usr/bin/env node
+process.removeAllListeners('warning')
+`.trim()
+
 /** @type {import('rollup').RollupOptions} */
 export default {
     input: 'src/index.ts',
     output: {
+        banner,
         file: pkg.main,
         format: 'cjs',
         sourcemap: true,
         exports: 'named',
     },
-    plugins: [typescript({target: 'es6'}), json(), cleanup({extensions: ['js', 'ts']}), shebang()],
+    plugins: [typescript({target: 'es6'}), json(), cleanup({extensions: ['js', 'ts']})],
     external,
 }
