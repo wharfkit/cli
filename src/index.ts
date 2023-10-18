6e445958967e7975a074dba4477af87600dd1ff6
dafuga <danielfugere28@gmail.com>
2023-10-17 19:12:58 -0700

    chore: added keys and account creation to wharfkit command

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,31 @@
-import {Command} from 'commander'
+import { Command } from 'commander';
 
-import {version} from '../package.json'
-import {generateContractFromCommand} from './commands/contract'
+import { version } from '../package.json';
+import { generateContractFromCommand } from './commands/contract';
+import { generateKeysFromCommand } from './commands/keys';
+import { createAccountFromCommand } from './commands/account';
 
-const program = new Command()
+const program = new Command();
 
-program.version(version).name('wharfkit').description('Wharf Command Line Utilities')
+program
+    .version(version)
+    .name('wharfkit')
+    .description('Wharf Command Line Utilities');
 
+// 1. Command to generate keys
+program
+    .command('generate keys')
+    .description('Generate a new set of public and private keys')
+    .action(createAccountFromCommand);
+
+// 2. Command to create an account
+program
+    .command('create account')
+    .description('Create a new account with an optional public key')
+    .option('--public-key <publicKey>', 'Public key for the new account. If not provided, keys are generated.')
+    .action(generateKeysFromCommand);
+
+// 3. Existing command to generate a contract
 program
     .command('generate')
     .description('Generate Contract Kit code for the named smart contract')
@@ -18,6 +37,6 @@ program
         'The URL of the API to connect with (e.g. "https://jungle4.greymass.com")',
         process.env.WHARFKIT_URL
     )
-    .action(generateContractFromCommand)
+    .action(generateContractFromCommand);
 
-program.parse()
+program.parse(process.argv);
